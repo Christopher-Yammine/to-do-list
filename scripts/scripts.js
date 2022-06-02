c4ac0bca555cb76a0df1ff8761442b304427516a
ChristopherYammine <chrisyammine98@gmail.com>
2022-06-02 15:30:31 +0300

    todo list layout and data storing

---
+++ scripts/scripts.js
@@ -0,0 +1,54 @@
+/*const myObject = {
+    name : "john doe",
+    age : 32,
+    gender : "male",
+    profession : "optician" 
+  }
+  
+  window.localStorage.setItem("myObject", JSON.stringify(myObject));
+  console.log(window.localStorage.getItem("myObject"));
+  let newObject = window.localStorage.getItem("myObject");
+  console.log(JSON.parse(newObject));
+  var d1 = new Date();*/
+  let counter=localStorage.length;
+  $("#btnadd")[0].addEventListener('click',saveValues)
+ function saveValues(){
+    let d1 = new Date();
+    date=[d1.getDate(),d1.getMonth()+1,d1.getFullYear(),d1.getMinutes(),d1.getHours()]
+    let id=Math.floor(Math.random() * 1000);
+    let title=$("#todotext")[0].value;
+    let rank=$("#importance")[0].value;
+    let description=$("#description")[0].value;
+    let isdone=0;
+    
+    let todoObject ={
+        todoId: id,
+        todoTitle:title,
+        todoRank:rank,
+        todoDesc:description,
+        todoDone:isdone,
+        todoDate:date
+    }
+    window.localStorage.setItem(counter, JSON.stringify(todoObject));
+    counter++;
+    displayUndone();
+    function displayUndone(){
+        let todos=$("#todos")[0];
+       todos.innerHTML='';
+       let tododiv='';
+       let todoslist=[];
+        for (let i=0;i<localStorage.length;i++){
+            todoslist.push((JSON.parse(window.localStorage.getItem(i))));
+            console.log(todoslist[i].todoTitle);
+
+            
+        }
+        
+        
+    
+    }
+    
+    //console.log(todoObject.todoDate[4]);
+    //console.log(title);
+    //$("#todotext")[0].value='';
+ }
